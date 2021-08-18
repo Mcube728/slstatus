8dff1cb92d6c304ae456f77468c6bdc927d56caa
Mcube728 <83777213+Mcube728@users.noreply.github.com>
2021-08-19 00:39:24 +0530

    made a few changes

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	
-    { cpu_perc, " [CPU :%s%%] ", NULL      },
-    { ram_perc, "[RAM :%s%%] ",  NULL      },
+    { cpu_perc, " [CPU: %s%%] ", NULL      },
+    { ram_perc, "[RAM: %s%%] ",  NULL      },
     { datetime, "%s",      "[%a %b %d %r] " },
 };
